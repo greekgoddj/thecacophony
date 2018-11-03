fa8c5d852f744d6aed623c65dc2fec0069738f2f
Max Hunt <max@huntw3.com>
2018-11-03 16:32:18 +0000

    Add user input upload and get routes.

--- django_server/thecacophony_server/game/urls.py
+++ django_server/thecacophony_server/game/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('uploaduserinput', views.upload_user_input, name="upload"),
+    path('getuserinputvalues', views.get_user_input_values, name="getuserinputvalues"),
 ]
