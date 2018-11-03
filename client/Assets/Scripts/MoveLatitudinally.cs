f7625a898fac966ffc7a90386863ebe0be2d47f2
NBull92 <nickbull@nickbull-computing.biz>
2018-11-03 18:40:06 +0000

    Take in speeds
    Take in speed through controls to affect the speed

--- client/Assets/Scripts/MoveLatitudinally.cs
+++ client/Assets/Scripts/MoveLatitudinally.cs
@@ -8,32 +8,69 @@ public class MoveLatitudinally : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
+    float number = 0;
+
 	// Update is called once per frame
 	void Update ()
     {
-        // Player 1
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * myMoveSpeed);
+            if(myMoveSpeed > -maxSpeed)
+                myMoveSpeed-= multiplier;
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * myMoveSpeed);
+            if(myMoveSpeed < maxSpeed)
+            {
+                myMoveSpeed += multiplier;
+            }
         }
 
-        // Player 2
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * myMoveSpeed);
+            if(myMoveSpeed > -maxSpeed)
+                myMoveSpeed -= multiplier;
         }
-
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * myMoveSpeed);
+            if (myMoveSpeed < maxSpeed)
+            {
+                myMoveSpeed += multiplier;
+            }
         }
+
+        //if (transform.position.x < 5.82f && transform.position.x > -5.82f)
+        //{
+            transform.Translate(Vector3.right * Time.deltaTime * myMoveSpeed);
+        //}
+
+        //// Player 1
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector3.left * Time.deltaTime * myMoveSpeed);
+        //}
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Translate(Vector3.right * Time.deltaTime * myMoveSpeed);
+        //}
+
+        // Player 2
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(Vector3.left * Time.deltaTime * myMoveSpeed);
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(Vector3.right * Time.deltaTime * myMoveSpeed);
+        //}
     }
 
     public float myMoveSpeed = 1;
+    public float maxSpeed = 10;
+    public float multiplier = 1;
+
 }
