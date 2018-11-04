5e127bf9428b669b8d9935031a30d7bc2c07aa61
Bea <beacrawford101@gmail.com>
2018-11-04 02:04:08 +0000

    Added scene to play with the audio

---
+++ client/Assets/TestAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour {
+
+    [Range(0.0f, 1.0f)]
+    public float alignment = 1.0f;
+    [Range(0.0f, 1.0f)]
+    public float maxOffset = 0.2f;
+    public AudioSource a, b;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = (1 - alignment) * maxOffset;
+        a.pitch = 1.0f - offset;
+        b.pitch = 1.0f - offset;
+        b.volume = 0;
+	}
+}
